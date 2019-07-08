d620a75513e9346966b72a9099a9d681f14ce230
Konstantin Zolotarev <konstantin.zolotarev@gmail.com>
2019-07-08 16:59:28 +0200

    STAX-33 moved to elixir 1.9

--- apps/docker/mix.exs
+++ apps/docker/mix.exs
@@ -9,7 +9,7 @@ defmodule Docker.MixProject do
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
       lockfile: "../../mix.lock",
-      elixir: "~> 1.7",
+      elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
       deps: deps()
