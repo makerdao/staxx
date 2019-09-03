0e241b07611bda84374fbcb784c094f4bb90577c
Konstantin Zolotarev <konstantin.zolotarev@gmail.com>
2019-09-03 22:11:28 +0300

    STAX-26 added new dets DB and helper for mapping user

---
+++ apps/deployment_scope/lib/deployment_scope/user_scope.ex
@@ -0,0 +1,95 @@
+defmodule Staxx.DeploymentScope.UserScope do
+  @moduledoc """
+  Helper module for mapping between user <-> deployment_scope
+  """
+  use GenServer
+
+  require Logger
+
+  @table "user_deployment_scope"
+
+  @doc false
+  def start_link(_) do
+    GenServer.start_link(__MODULE__, :ok, name: __MODULE__)
+  end
+
+  @doc false
+  def init(:ok) do
+    :dets.open_file(table(), type: :bag)
+    Logger.debug(fn -> "Started new DETS table for mapping user <-> scope" end)
+    {:ok, @table}
+  end
+
+  @doc false
+  def terminate(_reason, _state) do
+    Logger.debug(fn -> "Stopping DETS table for mapping user -> scope" end)
+    :dets.close(table())
+  end
+
+  @doc false
+  def handle_call({:map, scope_id, email}, _, state) do
+    case :dets.insert_new(table(), {email, scope_id}) do
+      true ->
+        {:reply, :ok, state}
+
+      false ->
+        {:reply, :ok, state}
+
+      {:error, err} ->
+        {:reply, {:error, err}, state}
+    end
+  end
+
+  @doc false
+  def handle_call({:by_email, email}, _, state) do
+    res =
+      table()
+      |> :dets.lookup(email)
+      |> Enum.map(fn {_, id} -> id end)
+
+    {:reply, res, state}
+  end
+
+  @doc false
+  def handle_cast({:unmap, scope_id, email}, state) do
+    :dets.delete_object(table(), {email, scope_id})
+    {:noreply, state}
+  end
+
+  @doc """
+  Maps given deployment scope with emial address
+  """
+  @spec map(binary, binary) :: :ok | {:error, term()}
+  def map(scope_id, email),
+    do: GenServer.call(__MODULE__, {:map, scope_id, email})
+
+  @doc """
+  Remove mapping between given deployment scope and user
+  This function ignores any error if no mapping is found
+  """
+  @spec unmap(binary, binary) :: :ok
+  def unmap(scope_id, email),
+    do: GenServer.cast(__MODULE__, {:unmap, scope_id, email})
+
+  @doc """
+  List all deployment scopes that are mapped to given user
+  """
+  @spec list_by_email(binary) :: [binary]
+  def list_by_email(email),
+    do: GenServer.call(__MODULE__, {:by_email, email})
+
+
+  # get path to DETS file for storage chain process
+  defp db_path() do
+    :proxy
+    |> Application.get_env(:dets_db_path)
+    |> Path.expand()
+  end
+
+  # Get full table path
+  defp table() do
+    db_path()
+    |> Path.join(@table)
+    |> String.to_atom()
+  end
+end
---
+++ apps/deployment_scope/test/deployment_scope/user_scope_test.exs
@@ -0,0 +1,22 @@
+defmodule Staxx.DeploymentScope.UserScopeTest do
+  use ExUnit.Case
+
+  alias Staxx.DeploymentScope.UserScope
+
+  test "do map with email" do
+    email = Faker.Internet.email()
+    id = Faker.String.base64()
+
+    assert [] = UserScope.list_by_email(email)
+
+    :ok = UserScope.map(id, email)
+    assert [^id] = UserScope.list_by_email(email)
+
+    # ignore duplicates
+    :ok = UserScope.map(id, email)
+    assert [^id] = UserScope.list_by_email(email)
+
+    :ok = UserScope.unmap(id, email)
+    assert [] = UserScope.list_by_email(email)
+  end
+end
