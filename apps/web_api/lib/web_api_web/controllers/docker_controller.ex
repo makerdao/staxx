6378a52ac2fc03ca323eaa2ddd2234e8631a9923
Konstantin Zolotarev <konstantin.zolotarev@gmail.com>
2020-06-18 15:07:19 +0300

    Updated postman collections

--- apps/web_api/lib/web_api_web/controllers/docker_controller.ex
+++ apps/web_api/lib/web_api_web/controllers/docker_controller.ex
@@ -12,14 +12,14 @@ defmodule Staxx.WebApiWeb.DockerController do
   alias Staxx.Environment
   alias Staxx.WebApiWeb.Schemas.DockerSchema
 
-  def start(conn, %{"extension_id" => id, "extension_name" => extension_name} = params) do
+  def start(conn, %{"environment_id" => id, "extension_name" => extension_name} = params) do
     # We wouldn't let users to control `rm` flag for container
     # Otherwise we will have a log of dead containers in our system
 
     with :ok <- DockerSchema.validate(params),
          container <- Container.create_container(params, id),
          {:ok, container} <- Environment.start_container(id, extension_name, container) do
-      Logger.debug("Extension #{id} Starting new docker container #{inspect(container)}")
+      Logger.debug("Environment #{id}: Starting new docker container #{inspect(container)}")
 
       conn
       |> put_status(200)
